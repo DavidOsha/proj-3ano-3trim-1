2cee11836f3fa27de1f7f7f1b46295506c922c1a
DavidOsha <134282397+DavidOsha@users.noreply.github.com>
2024-11-11 09:56:27 -0300

    Add files via upload

--- projetocienciadados/graficos/informacoesGlobais.js
+++ projetocienciadados/graficos/informacoesGlobais.js
@@ -1,3 +1,11 @@
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
-console.log(url)
+async function vizualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    const dados = await res.json()
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container')
+    //console.log(dados);
+}
+
+vizualizarInformacoesGlobais()
